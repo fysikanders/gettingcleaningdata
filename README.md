f8a5f493dba06cae7f448287ef5028c6a856b65d
Anders Martensson <fysikanders@gmail.com>
2017-10-07 23:36:28 +0200

    Finished the scripts

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
 Getting and cleaning data
-==================
+=========================
+Project to extract and tidy up a data set from a smart phone.
+This is part of 'Getting and Cleaning data' course on Coursera.
+
+How to run
+==========
+To download the data, source (execute) the script **downloadDataSet.R**
+This should only be needed once.
+
+To run the analysis and write the output data set, source (execute) the script **run_analysis.R**
+
+
+Feature description
+===================
+The measurements have been split into 2 main parts:
+1. How much is the body accelerating
+2. How much of the acceleration is gravity 
+
+The output data set includes only mean values and standard deviations of the raw data. 
+
+All features with starting with **Body** are measurements of the body acceleration and all features starting with **Gravity** are measurements of gravity.
+
+The data also includes frequency analysis and these features start with **Frequency**. The datapoints are actuallt Fast-Fourier-Transforms of the data points in the time domain.
+
+The column *Subject* states which test subject the measurement was colleded on.
+
+The column *ActivityName* states which activity the subject was performing when the measurment was taken.
+
+
+Raw data
+========
+A full description is available at the site where the data was obtained:
+http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+
+Here are the data for the project:
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
 
--- run_analysis.R
+++ run_analysis.R
@@ -2,27 +2,32 @@
 
 column_names <- read.csv("UCI HAR Dataset/features.txt", sep = "", header = FALSE, colClasses = c("numeric", "character"))
 
-# Read training set and merge with activity
+# Read training set and merge with subject and activity
 x_train <- read.csv("UCI HAR Dataset/train/X_train.txt", sep = "", header = FALSE, colClasses = c(rep("numeric", 561)))
 names(x_train) = column_names[,2]
-y_train <- read.csv("UCI HAR Dataset/train/y_train.txt", sep = "", header = FALSE)
+s_train <- read.csv("UCI HAR Dataset/train/subject_train.txt", sep = "", header = FALSE, colClasses = c("numeric"))
+names(s_train) = c("Subject")
+y_train <- read.csv("UCI HAR Dataset/train/y_train.txt", sep = "", header = FALSE, colClasses = c("numeric"))
 names(y_train) = c("Activity")
-train <- cbind(x_train, y_train)
+train <- cbind(x_train, s_train, y_train)
 
-rm(list = c("x_train", "y_train")) # clean up
+rm(list = c("x_train", "s_train", "y_train")) # clean up
 
 
-# Read test set and merge with activity
-x_test <- read.csv("UCI HAR Dataset/test/X_test.txt", sep = "", header = FALSE)
+# Read test set and merge with subject and activity
+x_test <- read.csv("UCI HAR Dataset/test/X_test.txt", sep = "", header = FALSE, colClasses = c(rep("numeric", 561)))
 names(x_test) = column_names[,2]
-y_test <- read.csv("UCI HAR Dataset/test/y_test.txt", sep = "", header = FALSE)
+s_test <- read.csv("UCI HAR Dataset/test/subject_test.txt", sep = "", header = FALSE, colClasses = c("numeric"))
+names(s_test) = c("Subject")
+y_test <- read.csv("UCI HAR Dataset/test/y_test.txt", sep = "", header = FALSE, colClasses = c("numeric"))
 names(y_test) = c("Activity")
-test <- cbind(x_test, y_test)
+test <- cbind(x_test, s_test, y_test)
 
-rm(list = c("x_test", "y_test")) # clean up
+rm(list = c("x_test", "s_test", "y_test")) # clean up
 
-# update columne names
-column_names <- rbind(column_names, c(562, "Activity"))
+# update column names
+column_names <- rbind(column_names, c(562, "Subject"))
+column_names <- rbind(column_names, c(563, "Activity"))
 
 # Merge training with test set
 full_set <- rbind(train, test)
@@ -31,11 +36,94 @@ full_set <- rbind(train, test)
 full_set <- full_set[!duplicated(column_names[,2])]
 
 # Extract mean and std
-mean_and_std_set <- select(full_set, matches("mean\\(\\)|std\\(\\)|Activity"))
+sensor_data <- select(full_set, matches("mean\\(\\)|std\\(\\)|Subject|Activity"))
 
 rm(list = c("full_set", "test", "train")) # clean up
 
 # Merge with activity labels
-activity_labels <- read.csv("UCI HAR Dataset/activity_labels.txt", sep = "", header = FALSE)
+#activity_labels <- read.csv("UCI HAR Dataset/activity_labels.txt", sep = "", header = FALSE, colClasses = c("numeric", "character"))
 
+sensor_data <- mutate(sensor_data, ActivityName = recode(sensor_data$Activity, `1` = "WALKING", `2` = "WALKING_UPSTAIRS", `3` = "WALKING_DOWNSTARIS", `4` = "SITTING", `5`= "STANDING", `6` = "LAYING"))
 
+# Set descriptive column names
+sensor_colnames <- c(
+  "BodyAccMeanX",
+  "BodyAccMeanY",
+  "BodyAccMeanZ",
+  "BodyAccStdX",
+  "BodyAccStdY",
+  "BodyAccStdZ",
+  "GravityAccMeanX",
+  "GravityAccMeanY",
+  "GravityAccMeanZ",
+  "GravityAccStdX",
+  "GravityAccStdY",
+  "GravityAccStdZ",
+  "BodyAccJerkMeanX",
+  "BodyAccJerkMeanY",
+  "BodyAccJerkMeanZ",
+  "BodyAccJerkStdX",
+  "BodyAccJerkStdY",
+  "BodyAccJerkStdZ",
+  "BodyGyroMeanX",
+  "BodyGyroMeanY",
+  "BodyGyroMeanZ",
+  "BodyGyroStdX",
+  "BodyGyroStdY",
+  "BodyGyroStdZ",
+  "BodyGyroJerkMeanX",
+  "BodyGyroJerkMeanY",
+  "BodyGyroJerkMeanZ",
+  "BodyGyroJerkStdX",
+  "BodyGyroJerkStdY",
+  "BodyGyroJerkStdZ",
+  "BodyAccMagMean",
+  "BodyAccMagStd",
+  "GravityAccMagMean",
+  "GravityAccMagStd",
+  "BodyAccJerkMagMean",
+  "BodyAccJerkMagStd",
+  "BodyGyroMagMean",
+  "BodyGyroMagStd",
+  "BodyGyroJerkMagMean",
+  "BodyGyroJerkMagStd",
+  "FrequencyBodyAccMeanX",
+  "FrequencyBodyAccMeanY",
+  "FrequencyBodyAccMeanZ",
+  "FrequencyBodyAccStdX",
+  "FrequencyBodyAccStdY",
+  "FrequencyBodyAccStdZ",
+  "FrequencyBodyAccJerkMeanX",
+  "FrequencyBodyAccJerkMeanY",
+  "FrequencyBodyAccJerkMeanZ",
+  "FrequencyBodyAccJerkStdX",
+  "FrequencyBodyAccJerkStdY",
+  "FrequencyBodyAccJerkStdZ",
+  "FrequencyBodyGyroMeanX",
+  "FrequencyBodyGyroMeanY",
+  "FrequencyBodyGyroMeanZ",
+  "FrequencyBodyGyroStdX",
+  "FrequencyBodyGyroStdY",
+  "FrequencyBodyGyroStdZ",
+  "FrequencyBodyAccMagMean",
+  "FrequencyBodyAccMagStd",
+  "FrequencyBodyBodyAccJerkMagMean",
+  "FrequencyBodyBodyAccJerkMagStd",
+  "FrequencyBodyBodyGyroMagMean",
+  "FrequencyBodyBodyGyroMagStd",
+  "FrequencyBodyBodyGyroJerkMagMean",
+  "FrequencyBodyBodyGyroJerkMagStd",
+  "Subject",
+  "Activity",
+  "ActivityName"
+)
+
+colnames(sensor_data) <- sensor_colnames
+
+# Group data according to subject and activity
+sensor_data_grouped <- group_by(sensor_data, Subject, ActivityName)
+
+output_set <- summarise_all(sensor_data_grouped, funs(mean))
+
+# Write output_set to file
+write.csv(output_set, file = "sensor_set.csv", row.names = FALSE)
